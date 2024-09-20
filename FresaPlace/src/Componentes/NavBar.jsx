dacfee65d5f7ffa6ad8525dd4f3d2c544e726ba9
MarilaMorales <mmorales@fwdcostarica.com>
2024-09-20 14:37:29 -0600

    NavBar ya sirve

--- FresaPlace/src/Componentes/NavBar.jsx
+++ FresaPlace/src/Componentes/NavBar.jsx
@@ -3,7 +3,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import "../styles/NavBar.css"
-import { Link } from 'react-router-dom';
+
 
 
 
@@ -13,29 +13,31 @@ function NavBarInc() {
 
 
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container id='back_color'>
-        <Navbar.Brand href="/Principal">FRESA PLACE</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/Escolar">Escolar</Nav.Link>
-            <Nav.Link href="/Decoracion">Decoracion</Nav.Link>
-            <Nav.Link href="/Adornos">Adornos</Nav.Link>
+ 
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container fluid id='back_color'>
+          <Navbar.Brand href="/Principal">FRESA PLACE</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/Escolar">Escolar</Nav.Link>
+              <Nav.Link href="/Decoracion">Decoracion</Nav.Link>
+              <Nav.Link href="/Adornos">Adornos</Nav.Link>
 
-            <NavDropdown title="Articulos de Fiesta" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Paquetes</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.1">Piñatas</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.1">Murales</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Cotice
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+              <NavDropdown title="Articulos de Fiesta" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Paquetes</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.1">Piñatas</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.1">Murales</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Cotice
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+  
   );
 }
 
